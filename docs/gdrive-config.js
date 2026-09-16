f2fb848d858afda74f8c369068ef9f6a77a0444b
hydra94501 <hydra94501@gmail.com>
2026-09-16 17:57:10 +0800

    Create gdrive-config.js

---
+++ docs/gdrive-config.js
@@ -0,0 +1,5 @@
+/* Google Drive API 配置 */
+var GDRIVE_CONFIG = {
+  API_KEY: 'AIzaSyAjkIyTML38cXy6sWwiQS32RbtEnShFfnY',
+  CLIENT_ID: '148495229560-ih20ugtglpai6n5tms8j9oih91tp3j82.apps.googleusercontent.com'
+};
